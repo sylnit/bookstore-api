d1926865df5f814d8e6d6f610c04831f2164a115
Samuel C. Iheadindu <samuel.iheadindu@abujaelectricity.com>
2019-12-23 12:05:23 +0100

    Create book and user controllers

---
+++ src/controllers/bookController.js
@@ -0,0 +1,78 @@
+import Book from "../models/book";
+import Author from "../models/author";
+import { getUserDetailsFromToken, getTokenFromHeader } from "../helpers/util";
+
+const getBooks = (req, res) => {
+	const { isbn, author } = req.query;
+	if (!isbn && !author) {
+		//get all books
+		Book.find({}, (err, books) => {
+			if (err) return res.status(400).json({ error: err });
+			res.status(200).json({ msg: "Books successfully retrieved", books });
+		});
+	} else if (isbn || author) {
+		//search with these fields
+		Book.find({ $or: [{ isbn: isbn }, { author: author }] }, (err, books) => {
+			if (err) return res.status(400).json({ error: err });
+			res.status(200).json({ msg: "Books successfully retrieved", books });
+		});
+	}
+};
+
+const createBook = (req, res) => {
+	const { isbn, title, publish_date } = req.body;
+	const token = getTokenFromHeader(req);
+	//get author from token
+	const { user_id } = getUserDetailsFromToken(token);
+	if (!user_id)
+		return res.status(400).json({ msg: "Invalid user. Process aborted." });
+	//get author
+	Author.findOne({ user: user_id }, (err, author) => {
+		if (err) return res.status(400).json({ error: err });
+		if (author) {
+			//use the author details to create the book
+			const authorId = author._id;
+			const newBook = new Book({ isbn, author: authorId, title, publish_date });
+			newBook
+				.save()
+				.then(data => {
+					res.status(200).json({ msg: "Book successfully created.", data });
+				})
+				.catch(err => {
+					res.status(400).json({ error: err });
+				});
+		} else {
+			res.status(404).json({ msg: "Author not found." });
+		}
+	});
+};
+
+const updateBook = (req, res) => {
+	const query = { _id: req.params.bookId };
+	Book.findOneAndUpdate(query, req.body, { upsert: true }, (err, book) => {
+		if (err) return res.status(400).json({ error: err });
+		res.status(200).json({ msg: "Book successfully updated.", data: book });
+	});
+};
+
+const deleteBook = (req, res) => {
+	const query = { _id: req.params.bookId };
+	const token = getTokenFromHeader(req);
+	const { author_id } = getUserDetailsFromToken(token);
+	Book.findOne(query, (err, book) => {
+		if (err) return res.status(400).json({ error: err });
+		if (book.author != author_id) {
+			return res
+				.status(400)
+				.json({ msg: "You can only delete books created by you." });
+		}
+		Book.deleteOne(query, err => {
+			if (err) return res.status(400).json({ error: err });
+			res
+				.status(200)
+				.json({ msg: `Book with ID: ${req.params.bookId} has been deleted` });
+		});
+	});
+};
+
+export default { getBooks, createBook, updateBook, deleteBook };
---
+++ src/controllers/userController.js
@@ -0,0 +1,72 @@
+import User from "../models/user";
+import { signToken } from "../helpers/util";
+import Author from "../models/author";
+
+const createUser = (req, res) => {
+	const {
+		username,
+		password,
+		user_type,
+		firstName,
+		lastName,
+		emailAddress
+	} = req.body;
+	const newUser = new User({ username, password, user_type });
+	newUser
+		.save()
+		.then(data => {
+			if (user_type == "AUTHOR") {
+				const user = data._id;
+				const newAuthor = new Author({
+					firstName,
+					lastName,
+					emailAddress,
+					user
+				});
+				newAuthor.save(function(err) {
+					if (err) res.status(400).json({ error: err });
+				});
+			}
+			res.status(200).json({ msg: "User successfully create.", data });
+		})
+		.catch(err => {
+			res.status(400).json({ msg: "User creation failed.", error: err });
+		});
+};
+
+const login = (req, res) => {
+	User.findOne({ username: req.body.username }, (err, user) => {
+		if (err) return res.status(400).json({ error: err });
+		if (!user) return res.status(404).json({ msg: "User not found." });
+
+		user.comparePassword(req.body.password, async (err, isMatch) => {
+			if (err) throw err;
+			if (!isMatch)
+				return res
+					.status(400)
+					.json({ msg: "Invalid username and / or password" });
+			const userType = user.user_type;
+			const userId = user._id;
+			let authorId;
+			await Author.findOne({ user: userId }, (err, author) => {
+				if (!err) {
+					if (author) {
+						authorId = author._id;
+					}
+				}
+			});
+			const authToken = signToken({
+				username: req.body.username,
+				user_type: userType,
+				user_id: userId,
+				author_id: authorId
+			});
+			res.status(200).json({
+				msg: "Login successful",
+				data: { token: authToken, user: user }
+			});
+		});
+	});
+};
+
+export default { createUser, login };
